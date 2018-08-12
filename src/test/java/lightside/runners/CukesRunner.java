3fc33284024c3a63f3419c54bc370e104f792bd7
shukhrataliyev <shukhrataliyev@gmail.com>
2018-08-11 22:44:08 -0500

    created project

---
+++ src/test/java/lightside/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package lightside.runners;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = {
+				"pretty",
+				"html:target/default-cucumber-reports",
+				"json:target/cucumber.json"
+		
+		},
+		
+		tags="@temp",
+		features="src/test/resources/darkside/features", 
+		glue="darkside/step_definitions"
+		,dryRun = false
+		)
+public class CukesRunner {}
